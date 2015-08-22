0a116c47b4e3ae441050fe924041021fdbd31568
Adam Brykajlo <adam.brykajlo@gmail.com>
2015-08-21 19:53:49 -0700

    added test suite skeleton

---
+++ tasks/tasks_test.go
@@ -0,0 +1,28 @@
+package tasks
+
+import (
+	"testing"
+	"net/http"
+)
+
+var client = &http.Client{}
+
+func TestPut(t *testing.T) {
+
+}
+
+func TestGet(t *testing.T) {
+
+}
+
+func TestDelete(t *testing.T) {
+
+}
+
+func TestPost(t *testing.T) {
+
+}
+
+func TestMain(t *testing.T) {
+	
+}
